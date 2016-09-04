ceb624affc16babc2c5135222ddd56ad402de628
Kabir Goel <kabirgoel.kg@gmail.com>
2016-09-04 10:17:51 +0530

    Added all kinds of neat features to update script

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # This script gathers my dotfiles and pastes them into the dotfiles folder.
 
-configuration() {
-    # change the following variable to where you store your dotfile binaries
-    # eg. ~/bin or /usr/local/bin
-    bin=~/bin
+# change the following variable to where you store your dotfile binaries
+# eg. ~/bin or /usr/local/bin
+bin=~/bin
+
+green() {
+    GREEN='\033[0;34m'
+    printf "${GREEN}${1}${NC}\n"
 }
 
 gather_binaries() {
@@ -33,7 +36,41 @@ git_push() {
     git push origin master
 }
 
-configuration
+diff_dotfiles() {
+    cd bin
+    for file in $(dir -1); do
+        green "$bin/$file"
+        diff "$bin/$file" ./$file
+    done
+    cd ..
+
+    green "~/.config/admiral.d/admiral.toml"
+    diff ~/.config/admiral.d/admiral.toml admiral.d/admiral.toml
+    green "~/.config/compton.conf"
+    diff ~/.config/compton.conf compton.conf
+    green "~/.config/nvim/init.vim"
+    diff ~/.config/nvim/init.vim nvim/init.vim
+    green "~/.config/gtk-3.0/gtk.css"
+    diff ~/.config/gtk-3.0/gtk.css gtk-3.0/gtk.css
+    green "~/.config/dunst/dunstrc"
+    diff ~/.config/dunst/dunstrc dunst/dunstrc
+    for file in $(dir -1 ~/.config/termite); do
+        green "~/.config/termite/$file"
+        diff ~/.config/termite/$file termite/$file
+    done
+    green "~/.config/bspwm/bspwmrc"
+    diff ~/.config/bspwm/bspwmrc bspwm/bspwmrc
+    green "~/.config/sxhkd/sxhkdrc"
+    diff ~/.config/sxhkd/sxhkdrc sxhkd/sxhkdrc
+    green "~/.weechat/weechat.conf"
+    diff ~/.weechat/weechat.conf weechat.conf
+}
+
+if [[ $1 == "-d" ]] || [[ $1 == "--diff" ]]; then
+    diff_dotfiles
+    exit
+fi
+
 gather_binaries
 gather_configs
 
